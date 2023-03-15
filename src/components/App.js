cb5448638c824bdad1e1ca5fce677ec8dd8ca508
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 02:07:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import {
+	BrowserRouter,
+	Route,
+	Routes,
+	useSearchParams,
+} from 'react-router-dom';
 import '../styles/App.css';
 
+const Details = () => {
+	const [searchParams] = useSearchParams();
+	return (
+		<div className="details">
+			Employee {searchParams.get('id')} named {searchParams.get('name')}{' '}
+			works as {searchParams.get('designation')}
+		</div>
+	);
+};
+
 const App = () => {
-  //Write your code here
-}
+	//Write your code here
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Details />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;
